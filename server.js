a29d8f430e7768843c3671c7c3633694c1d87d1a
Zacky Ma <zacky@marchbox.com>
2026-05-23 10:57:59 -0700

    initial code

---
+++ server.js
@@ -0,0 +1,64 @@
+import { createServer } from "node:http";
+
+function wait(ms) {
+  const { promise, resolve } = Promise.withResolvers();
+  setTimeout(resolve, ms);
+  return promise;
+}
+
+const server = createServer(async (_, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html; charset=utf-8",
+    "Transfer-Encoding": "chunked",
+  });
+
+  res.write(`
+    <!doctype html>
+    <html lang="en">
+    <meta charset="utf-8">
+    <title>HTML streaming test</title>
+    <p>Hello…</p>
+  `);
+
+  await wait(3000);
+
+  res.write(`
+    <div>
+  `);
+
+  await wait(3000);
+
+  res.write(`
+    <template shadowrootmode="open">
+  `);
+
+  await wait(3000);
+
+  res.write(`
+    <span>world!</span>
+  `);
+
+  await wait(3000);
+
+  res.write(`
+    </template>
+  `);
+
+  await wait(3000);
+
+  res.write(`
+    </div>
+  `);
+
+  await wait(3000);
+
+  res.end(`
+    <footer>The end</footer>
+    </html>
+  `);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
